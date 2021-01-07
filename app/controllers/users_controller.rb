f084992a6ad62fb86d656ef3b4f5b65471ff71f2
Shunpei Sakiyama <s3s15s3s@gmail.com>
2021-01-07 08:20:55 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
-  def new
+  # GET users/:id
+  def show
+    @user = User.find(params[:id])
   end
+  
+  # GET users/new
+  def new
+    @user = User.new
+  end
+  
+  #POST users (+ params)
+  def create
+   #Strong Parametersの実装  
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      #succes
+      redirect_to @user
+      # GET "/users/#{@user.id}"
+    else
+     render 'new'
+      #failer
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
