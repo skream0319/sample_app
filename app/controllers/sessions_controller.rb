b00f6f2a180f2a634aa45c4993d09e2a13dac0d2
Shunpei Sakiyama <s3s15s3s@gmail.com>
2021-02-04 13:54:26 +0000

    Finish ch12

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-  end
+ end
 
   def destroy
     log_out if logged_in?
