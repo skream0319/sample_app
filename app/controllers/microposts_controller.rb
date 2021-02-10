9f71535604c1e96a3c0c1bb71478b19d27288a12
Shunpei Sakiyama <s3s15s3s@gmail.com>
2021-02-10 10:55:42 +0000

    Finish cp13

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,6 +4,8 @@ class MicropostsController < ApplicationController
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
+    # Actuve Storageのインストールにより"attachメソッド"が使える
+    @micropost.image.attach(params[:micropost][:image])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
@@ -22,7 +24,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content, :image)
     end
     
     def correct_user
