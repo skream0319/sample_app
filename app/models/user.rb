e510205099df057010bf523fdf0eee840cf71054
Shunpei Sakiyama <s3s15s3s@gmail.com>
2021-02-10 04:47:07 +0000

    Finish ch13 except 13.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token,
                 :activation_token,
                 :reset_token
@@ -76,6 +76,12 @@ class User < ApplicationRecord
   # パスワード再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     self.reset_sent_at < 2.hours.ago
+  end
+  
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", self.id)
   end
   
   #　非公開
